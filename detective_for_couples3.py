7ce1496d07ff444a5a93f2702dacc4ab6970fc2c
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-03-02 15:40:14 +0200

    15.30

--- detective_for_couples3.py
+++ detective_for_couples3.py
@@ -1,18 +1,19 @@
 import datetime
 from collections import defaultdict
 
+
 def read_data(file_path):
     data = []
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         device = {}
         for line in f:
             line = line.strip()
-            if line.startswith('==='):
+            if line.startswith("==="):
                 if device:
                     data.append(device)
                     device = {}
             elif line:
-                key, value = line.split(':', 1)
+                key, value = line.split(":", 1)
                 device[key.strip()] = value.strip()
         if device:
             data.append(device)
@@ -21,6 +22,7 @@ def read_data(file_path):
         row["last"] = datetime.datetime.strptime(row["last"], "%d.%m.%Y %H:%M:%S")
         row["count"] = int(row["count"])
     return data
+
 
 def get_paired_devices(data):
     paired_devices = defaultdict(list)
@@ -34,12 +36,14 @@ def get_paired_devices(data):
                 paired_devices[data[j]["mac"]].append(data[i])
     return paired_devices
 
+
 def count_device_pairs(paired_devices):
     count_dict = defaultdict(int)
     for dev, paired in paired_devices.items():
         for pair in paired:
             count_dict[tuple(sorted([dev, pair["mac"]]))] += 1
     return count_dict
+
 
 def get_together_devices(count_dict):
     together = []
@@ -49,6 +53,7 @@ def get_together_devices(count_dict):
     together = sorted(set(together), key=lambda x: together.count(x), reverse=True)
     return together
 
+
 def get_paired_info(data, paired_devices):
     paired_info = []
     for dev in data:
@@ -56,8 +61,19 @@ def get_paired_info(data, paired_devices):
             pair for pair in paired_devices[dev["mac"]] if pair["user"] != dev["user"]
         ]
         if paired_with:
-            paired_info.append((dev["user"], dev["first"], dev["last"], dev["count"], dev["mac"], dev["ip"], paired_with))
+            paired_info.append(
+                (
+                    dev["user"],
+                    dev["first"],
+                    dev["last"],
+                    dev["count"],
+                    dev["mac"],
+                    dev["ip"],
+                    paired_with,
+                )
+            )
     return paired_info
+
 
 def detective(user):
     data = read_data("C:/Users/r.manov/Desktop/Data.txt")
@@ -65,8 +81,13 @@ def detective(user):
     count_dict = count_device_pairs(paired_devices)
     together = get_together_devices(count_dict)
     paired_info = get_paired_info(data, paired_devices)
-    detected_devices = [(user, first, last, count, mac, ip, paired) for user, first, last, count, mac, ip, paired in paired_info if user == user and any(pair["user"] == user for pair in paired)]
+    detected_devices = [
+        (user, first, last, count, mac, ip, paired)
+        for user, first, last, count, mac, ip, paired in paired_info
+        if user == user and any(pair["user"] == user for pair in paired)
+    ]
     return detected_devices, together
+
 
 def main():
     detected_devices, together = detective("JULIA")
@@ -74,6 +95,7 @@ def main():
         print(user, first, last, count, mac, ip, [pair["user"] for pair in paired])
     print(*together, sep="\n")
     print(len(together))
+
 
 if __name__ == "__main__":
     main()
