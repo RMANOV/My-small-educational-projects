9d96e070a64974e210850a618ea0ebc12848b9d3
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-09-23 17:04:53 +0300

    feat(MangoSpider): Enhance data scraping and error handling
    - Add detailed comments and documentation to each function.
    - Implement ValueError for missing product attributes (name, price, color, sizes).
    - Prioritize discounted prices when available.
    - Improve logging to include the URL of the response in case of failure.
    
    This commit aims to make the code more robust, readable, and maintainable. It also better handles edge cases related to missing or special attributes of products.

--- scrapy_spider7.py
+++ scrapy_spider7.py
@@ -25,6 +25,8 @@ import scrapy
 import re
 
 class MangoSpider(scrapy.Spider):
+    """This spider scrapes product information from Mango's online store."""
+    
     name = 'mango'
     start_urls = [
         'https://shop.mango.com/bg-en/men/t-shirts-plain/100-linen-slim-fit-t-shirt_47095923.html?c=07',
@@ -32,16 +34,21 @@ class MangoSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        product = {
-            'name': self.get_product_name(response),
-            'price': self.get_product_price(response),
-            'color': self.get_product_color(response),
-            'sizes': self.get_product_sizes(response),
-        }
-        yield product
+        """Parse the HTML to extract product details."""
+        try:
+            product = {
+                'name': self.get_product_name(response),
+                'price': self.get_product_price(response),
+                'color': self.get_product_color(response),
+                'sizes': self.get_product_sizes(response),
+            }
+            yield product
+        except ValueError as e:
+            self.log(f"Failed to extract data from {response.url}: {e}")
 
     @staticmethod
     def get_product_name(response):
+        """Extract and return the product name, or raise ValueError."""
         name = response.css('h1.product-name::text').get()
         if name:
             return name.strip()
@@ -50,35 +57,34 @@ class MangoSpider(scrapy.Spider):
 
     @staticmethod
     def get_product_price(response):
+        """Extract and return the product price, or raise ValueError."""
         price_text = response.css('span.product-sale::text').get()
         discounted_price_text = response.css('span.product-discounted-price::text').get()
-
-        # If there's a discounted price, prioritize that
-        price_to_use = discounted_price_text or price_text
-
+        price_to_use = discounted_price_text or price_text  # Prioritize discounted price
         if price_to_use:
             match = re.search(r'\d+\.\d+', price_to_use)
             if match:
                 return float(match.group())
-        
         raise ValueError("Missing price")
 
     @staticmethod
     def get_product_color(response):
+        """Extract and return the product color(s), or raise ValueError."""
         colors = response.css('span.color-name::text').getall()
         if colors:
             return ", ".join(color.strip() for color in colors)
         else:
             raise ValueError("Missing color")
 
-
     @staticmethod
     def get_product_sizes(response):
+        """Extract and return the product sizes, or raise ValueError."""
         sizes = response.css('div.size-selector-container > div > ul > li > a::text').getall()
         if sizes:
             return list(filter(None, map(str.strip, sizes)))
         else:
             raise ValueError("Missing sizes")
+
 
 
 
