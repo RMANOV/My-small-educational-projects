1636b78119ab72f8824e61deb9c5696e1f0bb6b9
Ruslan Manov <R.MANOV@GMAIL.COM>
2024-03-04 23:02:37 +0200

    Add dynamic weight adjustment and brightness combination functions

--- adaptive_brightnes5.py
+++ adaptive_brightnes5.py
@@ -47,6 +47,66 @@ def get_screenshot_brightness():
     return brightness / 256 * 100
 
 
+# Инициализация на базови тегла
+weight_camera = 0.5
+weight_screenshot = 0.5
+
+def adjust_weights_based_on_content(camera_brightness, screenshot_brightness):
+    """
+    Динамично регулиране на теглата в зависимост от съотношението между яркостта, 
+    измерена от камерата, и яркостта на скрийншота.
+    """
+    # Определяне на съотношението между яркостта на скрийншота и яркостта от камерата
+    ratio = screenshot_brightness / camera_brightness if camera_brightness != 0 else 1
+
+
+
+    # # Динамично коригиране на теглата в зависимост от съотношението
+    # if ratio > 1.2:  # Екранът е значително по-ярък от околната осветеност
+    #     weight_camera = 0.3
+    #     weight_screenshot = 0.7
+    # elif ratio < 0.8:  # Екранът е значително по-тъмен от околната осветеност
+    #     weight_camera = 0.7
+    #     weight_screenshot = 0.3
+
+    min_weight = 0.3
+    max_weight = 0.7
+
+    if ratio > 1:
+        weight_camera = min_weight + (1 - min_weight) * (ratio - 1) / (max_ratio - 1)
+        weight_screenshot = 1 - weight_camera
+    elif ratio < 1:
+        weight_screenshot = min_weight + (1 - min_weight) * (1 - ratio) / (1 - min_ratio) 
+        weight_camera = 1 - weight_screenshot
+    else:
+        weight_camera = 0.5
+        weight_screenshot = 0.5
+
+    # brightness = weight_camera * camera_brightness + weight_screenshot * screenshot_brightness
+        
+
+    return weight_camera, weight_screenshot
+
+
+def combine_brightness(camera_brightness, screenshot_brightness, weight_camera, weight_screenshot):
+    """
+    Комбиниране на стойностите за яркост с използване на динамично регулирани тегла.
+    """
+    combined_brightness = (weight_camera * camera_brightness + weight_screenshot * (
+        100 / screenshot_brightness)) / (weight_camera + weight_screenshot)
+    return combined_brightness
+
+# Във функцията за регулиране на яркостта:
+    # Определяне на теглата
+    weight_camera, weight_screenshot = adjust_weights_based_on_content(
+        camera_brightness, screenshot_brightness)
+
+    # Комбиниране на яркостите с определените тегла
+    brightness = combine_brightness(
+        camera_brightness, screenshot_brightness, weight_camera, weight_screenshot)
+
+
+
 def adjust_screen_brightness(camera_index=0):
     # Получаване на текущата яркост на екрана
     prev_brightness = sbc.get_brightness()[0]
@@ -94,7 +154,11 @@ def adjust_screen_brightness(camera_index=0):
 
                 else:
                     # Комбиниране на двете стойности за яркост
-                    brightness = (camera_brightness + 1/screenshot_brightness) / 2
+                    # brightness = (camera_brightness + 1/screenshot_brightness) / 2
+                    brightness = combine_brightness(
+                        camera_brightness, screenshot_brightness, weight_camera, weight_screenshot)
+                    
+
 
                 
                 brightness_diff = abs(brightness - prev_brightness)
